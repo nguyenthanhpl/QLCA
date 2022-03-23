097f18885bfbff360ed48378c6a52adbdf1cd712
Sangg <sangtranhd20@gmail.com>
2022-03-23 23:01:43 +0700

    sang

---
+++ Quanlicaan/Quanlicaan/Controllers/DangkycanhanController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Quanlicaan.Controllers
+{
+    public class DangkycanhanController : Controller
+    {
+        // GET: Dangkycanhan
+        public ActionResult Index()
+        {
+            return View("dangkyancanhan");
+        }
+    }
+}
